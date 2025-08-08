c50fbcf0344953d8be0015ef6d54bde48d7ec862
Ilan Nowak--Glandier <inowak--@1F1.42angouleme.fr>
2025-08-08 20:14:21 +0200

    register | login | home | pong traduction

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,71 +1,105 @@
 export const translations = {
-    fr: {
-        // Navbar link
-        home: "Accueil",
-        settings: "Parametres",
-        shop: "Boutique",
-        inventory: "Inventaire",
-        //HomePage
-        welcome: "Bienvenue",
-        login: "Connexion",
-        logout: "Déconnexion",
-        register: "Inscription",
-        play: "Jouer",
-        // ShopPage
-        default_bar: "Barre de base",
-        fire_bar: "Barre en feu",
-        ice_bar: "Barre en glace",
-        amethyst_bar: "Barre en améthyste",
-        // Cosmetics
-        avatar: "Avatar",
-        gamebackground: "Fond du jeux",
-        bar: "Barre",
-        ball: "Balle",
-    },
-    en: {
-        // Navbar link
-        home: "Home",
-        settings: "Settings",
-        shop: "Shop",
-        inventory: "Inventory",
-        //HomePage
-        welcome: "Welcome",
-        login: "Login",
-        logout: "Logout",
-        register: "Register",
-        play: "Play",
-        // ShopPage
-        default_bar: "Default bar",
-        fire_bar: "Fire bar",
-        ice_bar: "Ice bar",
-        amethyst_bar: "Amethyst bar",
-        // Cosmetics
-        avatar: "Avatar",
-        gamebackground: "Game background",
-        bar: "Bar",
-        ball: "Ball",
-    },
-    es: {
-        // Navbar link
-        home: "Inicio",
-        settings: "Parámetro",
-        shop: "Tienda",
-        inventory: "Inventario",
-        //HomePage
-        welcome: "Bienvenido",
-        login: "Iniciar sesión",
-        logout: "Cerrar sesión",
-        register: "Registrarse",
-        play: "Jugar",
-        // ShopPage
-        default_bar: "Barra básica",
-        fire_bar: "Barra en llamas",
-        ice_bar: "Barra de hielo",
-        amethyst_bar: "Barra de amatista",
-        // Cosmetics
-        avatar: "Avatar",
-        gamebackground: "Fondo del juego",
-        bar: "Barra",
-        ball: "Bola",
-    }
+	fr: {
+		// Login
+		username: "Pseudonyme",
+		password: "Mots de passe",
+		confirm_password: "Confirmer le mots de passe",
+		go_to_register: "Aller a la page d'inscription",
+		// Register
+		back_to_login: "Retour a la page de login",
+		// Navbar link
+		home: "Accueil",
+		settings: "Parametres",
+		shop: "Boutique",
+		inventory: "Inventaire",
+		//HomePage
+		welcome: "Bienvenue",
+		login: "Connexion",
+		logout: "Déconnexion",
+		register: "Inscription",
+		play: "Jouer",
+		// ShopPage
+		default_bar: "Barre de base",
+		fire_bar: "Barre en feu",
+		ice_bar: "Barre en glace",
+		amethyst_bar: "Barre en améthyste",
+		// Cosmetics
+		avatar: "Avatar",
+		gamebackground: "Fond du jeux",
+		bar: "Barre",
+		ball: "Balle",
+		// Pong Menu
+		username1: "Joueur 1",
+		username2: "Joueur 2",
+		// Overlay
+		back_to_menu: "Retour au menu",
+	},
+	en: {
+		// Login
+		username: "Username",
+		password: "Password",
+		confirm_password: "Confirm password",
+		go_to_register: "Go to register",
+		// Register
+		back_to_login: "Back to login page",
+		// Navbar link
+		home: "Home",
+		settings: "Settings",
+		shop: "Shop",
+		inventory: "Inventory",
+		//HomePage
+		welcome: "Welcome",
+		login: "Login",
+		logout: "Logout",
+		register: "Register",
+		play: "Play",
+		// ShopPage
+		default_bar: "Default bar",
+		fire_bar: "Fire bar",
+		ice_bar: "Ice bar",
+		amethyst_bar: "Amethyst bar",
+		// Cosmetics
+		avatar: "Avatar",
+		gamebackground: "Game background",
+		bar: "Bar",
+		ball: "Ball",
+		username1: "Player1",
+		username2: "Player2",
+		// Overlay
+		back_to_menu: "Back to menu",
+	},
+	es: {
+		// Login
+		username: "Seudónimo",
+		password: "Contraseñas",
+		confirm_password: "Confirmar contraseña",
+		go_to_register: "Volver a la página de registrarse",
+		// Register
+		back_to_login: "Volver a la página de inicio de sesión",
+		// Navbar link
+		home: "Inicio",
+		inventory: "Inventario",
+		settings: "Parámetro",
+		shop: "Tienda",
+		//HomePage
+		welcome: "Bienvenido",
+		login: "Iniciar sesión",
+		logout: "Cerrar sesión",
+		register: "Registrarse",
+		play: "Jugar",
+		// ShopPage
+		default_bar: "Barra básica",
+		fire_bar: "Barra en llamas",
+		ice_bar: "Barra de hielo",
+		amethyst_bar: "Barra de amatista",
+		// Cosmetics
+		avatar: "Avatar",
+		gamebackground: "Fondo del juego",
+		bar: "Barra",
+		ball: "Bola",
+		username1: "Jugador 1",
+		username2: "Jugador 2",
+		// Overlay
+		back_to_menu: "	Volver al menú",
+	}
 };
