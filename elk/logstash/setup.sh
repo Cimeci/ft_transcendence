ccb2c960f11423e1df2ab4e143e052f321f843a6
antauber <antauber@Trixie.myguest.virtualbox.org>
2025-09-23 13:18:26 +0200

    Chore: remove debug code, setup logstash

--- elk/logstash/setup.sh
+++ elk/logstash/setup.sh
@@ -3,14 +3,6 @@ set -e
 
 export LOGSTASH_KEYSTORE_PASS=${LOGSTASH_KEYSTORE_PASS}
 
-echo "=== DEBUGGING ==="
-echo "User: $(whoami)"
-echo "ELASTIC_USER: $ELASTIC_USER"
-echo "ELASTIC_PASSWORD: $ELASTIC_PASSWORD" 
-echo "ELASTIC_HOSTS: $ELASTIC_HOSTS"
-echo "=================="
-
-
 if [ ! -f /usr/share/logstash/config/logstash.keystore ]; then
     echo "Creating Logstash keystore"
     echo "y" | bin/logstash-keystore --path.settings /usr/share/logstash/config create -p "${LOGSTASH_KEYSTORE_PASS}"
